50715510c6c44ff04f7043a385caf55c6345b871
jcohy <jia_chao23@126.com>
2020-04-21 19:04:19 +0800

    添加 redis.adoc

---
+++ spring-data/src/test/java/org/springframework/data/rest/tests/security/SecuredPersonRepository.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2014-2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package java.org.springframework.data.rest.tests.security;
+
+import java.util.UUID;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.security.access.annotation.Secured;
+
+// tag::code[]
+@Secured("ROLE_USER") // <1>
+@RepositoryRestResource(collectionResourceRel = "people", path = "people")
+public interface SecuredPersonRepository extends CrudRepository<Person, UUID> {
+
+	@Secured("ROLE_ADMIN") // <2>
+	@Override
+	void deleteById(UUID aLong);
+
+	@Secured("ROLE_ADMIN")
+	@Override
+	void delete(Person person);
+
+	@Secured("ROLE_ADMIN")
+	@Override
+	void deleteAll(Iterable<? extends Person> persons);
+
+	@Secured("ROLE_ADMIN")
+	@Override
+	void deleteAll();
+}
+// end::code[]
