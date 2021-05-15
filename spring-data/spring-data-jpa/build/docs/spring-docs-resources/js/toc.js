d41e9e89cc80f39aaed715a22ed7ddcb43e6330d
jiac <jia_chao23@126.com>
2021-05-15 18:40:48 +0800

    check spring-boot-rest

---
+++ spring-data/spring-data-jpa/build/docs/spring-docs-resources/js/toc.js
@@ -0,0 +1,44 @@
+var toctitle = document.getElementById('toctitle');
+var path = window.location.pathname;
+if (toctitle != null) {
+    var oldtoc = toctitle.nextElementSibling;
+    var newtoc = document.createElement('div');
+    newtoc.setAttribute('id', 'tocbot');
+    newtoc.setAttribute('class', 'js-toc desktop-toc');
+    oldtoc.setAttribute('class', 'mobile-toc');
+    oldtoc.parentNode.appendChild(newtoc);
+    tocbot.init({
+        contentSelector: '#content',
+        headingSelector: 'h1, h2, h3, h4, h5',
+        positionFixedSelector: 'body',
+        fixedSidebarOffset: 90,
+        smoothScroll: false
+    });
+    if (!path.endsWith("index.html") && !path.endsWith("/")) {
+        var link = document.createElement("a");
+        if (document.getElementById('index-link')) {
+          indexLinkElement = document.querySelector('#index-link > p > a');
+          linkHref = indexLinkElement.getAttribute("href");
+          link.setAttribute("href", linkHref);
+        } else {
+          link.setAttribute("href", "index.html");
+        }
+        link.innerHTML = "<span><i class=\"fa fa-chevron-left\" aria-hidden=\"true\"></i></span> Back to index";
+        var block = document.createElement("div");
+        block.setAttribute('class', 'back-action');
+        block.appendChild(link);
+        var toc = document.getElementById('toc');
+        var next = document.getElementById('toctitle').nextElementSibling;
+        toc.insertBefore(block, next);
+    }
+}
+
+var headerHtml = '<div id="header-spring">\n' +
+    '<h1>\n' +
+    '\n' +
+    '</h1>\n' +
+    '</div>';
+
+var header = document.createElement("div");
+header.innerHTML = headerHtml;
+document.body.insertBefore(header, document.body.firstChild);
