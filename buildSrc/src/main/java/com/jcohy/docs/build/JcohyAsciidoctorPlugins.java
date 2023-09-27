c370ff8d45948e17c1fee8cd76319565e3bec7dd
Jcohy <jia_chao23@126.com>
2023-09-27 15:39:06 +0800

    1、更新 jcohyGradleVersion 版本

--- buildSrc/src/main/java/com/jcohy/docs/build/JcohyAsciidoctorPlugins.java
+++ buildSrc/src/main/java/com/jcohy/docs/build/JcohyAsciidoctorPlugins.java
@@ -17,6 +17,7 @@ import org.asciidoctor.gradle.jvm.pdf.AsciidoctorPdfTask;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.Task;
+import org.gradle.api.file.DuplicatesStrategy;
 import org.gradle.api.file.FileCollection;
 import org.gradle.api.plugins.PluginContainer;
 import org.gradle.api.tasks.Copy;
@@ -33,7 +34,7 @@ import org.gradle.api.tasks.Sync;
  */
 public class JcohyAsciidoctorPlugins implements Plugin<Project> {
 
-	private final Set<String> excludePdfProject = Set.of("spring-security","spring-boot");
+	private final Set<String> excludePdfProject = Set.of("spring-security","spring-boot","spring-framework");
 
     @Override
     public void apply(Project project) {
@@ -43,60 +44,56 @@ public class JcohyAsciidoctorPlugins implements Plugin<Project> {
             version = ProjectVersion.getVersionFromName(project.getName());
         }
 		project.setVersion(version);
-		configureAsciidoctorTask(project);
 
-//		project.afterEvaluate(p -> p.getTasks().withType(AsciidoctorTask.class, asciidoctorTask -> {
-//
-//		}));
         plugins.apply(AsciidoctorJPlugin.class);
         plugins.apply(AsciidoctorConventionsPlugin.class);
         plugins.apply(ConventionsPlugin.class);
         plugins.apply(DeployedPlugin.class);
 
+		configureAsciidoctorTask(project);
+
 		createAggregatedTask(project);
     }
 
 	private void createAggregatedTask(Project project) {
-		project.afterEvaluate(p -> {
-			project.getTasks().create("aggregatedProject", Sync.class,aggregatedProject -> {
-				aggregatedProject.setGroup("documentation");
-				File syncSource = new File(project.getRootProject().getBuildDir(),"reference/");
+		File syncSource = new File(project.getRootProject().getBuildDir(),"reference/");
+		project.getTasks().create("aggregatedProject", Copy.class,aggregatedProject -> {
+			aggregatedProject.setGroup("documentation");
 
-				aggregatedProject.setDestinationDir(syncSource);
+			aggregatedProject.setDestinationDir(syncSource);
 
-				if(project.getTasks().getNames().contains("asciidoctor")) {
-					Task asciidoctor = project.getTasks().getByName("asciidoctor");
-					aggregatedProject.dependsOn(asciidoctor);
-					aggregatedProject.from(asciidoctor.getOutputs(),spec -> {
-						spec.into(project.getName()+"/"+project.getVersion()+"/htmlsingle");
+			if(project.getTasks().getNames().contains("asciidoctor")) {
+				Task asciidoctor = project.getTasks().getByName("asciidoctor");
+				aggregatedProject.dependsOn(asciidoctor);
+
+				aggregatedProject.from(asciidoctor.getOutputs(),spec -> {
+					spec.into(project.getName()+"/"+project.getVersion()+"/htmlsingle");
+				});
+			}
+
+			if(project.getTasks().getNames().contains("asciidoctorPdf")) {
+				if(!excludePdfProject.contains(project.getName())) {
+					Task asciidoctorPdf = project.getTasks().getByName("asciidoctorPdf");
+					aggregatedProject.dependsOn(asciidoctorPdf);
+					aggregatedProject.from(asciidoctorPdf.getOutputs(),spec -> {
+						spec.into(project.getName()+"/"+project.getVersion()+"/pdf");
 					});
 				}
+			}
 
-				if(project.getTasks().getNames().contains("asciidoctorPdf")) {
-					if(!excludePdfProject.contains(project.getName())) {
-						Task asciidoctorPdf = project.getTasks().getByName("asciidoctorPdf");
-						aggregatedProject.dependsOn(asciidoctorPdf);
-						aggregatedProject.from(asciidoctorPdf.getOutputs(),spec -> {
-							spec.into(project.getName()+"/"+project.getVersion()+"/pdf");
-						});
-					}
-				}
-
-				if(project.getTasks().getNames().contains("asciidoctorMultiPage")) {
-					Task multiPage = project.getTasks().getByName("asciidoctorMultiPage");
-					aggregatedProject.dependsOn(multiPage);
-					aggregatedProject.from(multiPage.getOutputs(),spec -> {
-						spec.into(project.getName()+"/"+project.getVersion()+"/html5");
-					});
-				}
-			});
+			if(project.getTasks().getNames().contains("asciidoctorMultiPage")) {
+				Task multiPage = project.getTasks().getByName("asciidoctorMultiPage");
+				aggregatedProject.dependsOn(multiPage);
+				aggregatedProject.from(multiPage.getOutputs(),spec -> {
+					spec.into(project.getName()+"/"+project.getVersion()+"/html5");
+				});
+			}
 		});
-
 	}
 
 	private void configureAsciidoctorTask(Project project) {
 		project.getTasks().withType(AsciidoctorTask.class,asciidoctorTask -> {
-			asciidoctorTask.languages("zh-cn");
+			asciidoctorTask.setLanguages(List.of("zh-cn"));
 			asciidoctorTask.setLogDocuments(true);
 			configureCommonAttributes(project, asciidoctorTask);
 			project.getExtensions().getByType(AsciidoctorJExtension.class).fatalWarnings(false);
@@ -126,16 +123,16 @@ public class JcohyAsciidoctorPlugins implements Plugin<Project> {
         attributes.put("image-resource", "https://resources.jcohy.com/jcohy-docs/images/" + ProjectVersion.getVersionfromAttr("spring-boot-version") + "/" + project.getName());
         attributes.put("spring-api-doc", "https://docs.spring.io/" + project.getName());
         attributes.put("doc-root","https://docs.jcohy.com");
+
+		attributes.put("docs-java",project.getProjectDir() + "/src/main/java/org/springframework/docs");
+		attributes.put("docs-kotlin",project.getProjectDir() + "/src/main/kotlin/org/springframework/docs");
+		attributes.put("docs-groovy",project.getProjectDir() + "/src/main/groovy/org/springframework/docs");
+
         if(project.getName().startsWith("spring-boot")) {
             attributes.put("docs-java",project.getProjectDir() + "/src/main/java/org/springframework/boot/docs");
             attributes.put("docs-kotlin",project.getProjectDir() + "/src/main/kotlin/org/springframework/boot/docs");
             attributes.put("docs-groovy",project.getProjectDir() + "/src/main/groovy/org/springframework/boot/docs");
-        } else {
-            attributes.put("docs-java",project.getProjectDir() + "/src/main/java/org/springframework/docs");
-            attributes.put("docs-kotlin",project.getProjectDir() + "/src/main/kotlin/org/springframework/docs");
-            attributes.put("docs-groovy",project.getProjectDir() + "/src/main/groovy/org/springframework/docs");
         }
-
         attributes.put("spring-docs-prefix","https://docs.spring.io/spring-framework/docs/");
         attributes.put("gh-samples-url","https://github.com/spring-projects/spring-security/master/");
         attributes.put("version",ProjectVersion.getVersionFromName(project.getName()));
